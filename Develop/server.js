a1e1aa846cb5e7f92aecf7295433004c5a63f0ce
Laura Aupert <aupert.laura@gmail.com>
2021-02-01 01:35:37 -0500

    Files: With server.js in the right folder this time.

---
+++ Develop/server.js
@@ -0,0 +1,8 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
