7a4f8c067faf934304d2a18378fce6ddf681dcdb
Laura Aupert <aupert.laura@gmail.com>
2021-02-01 02:18:01 -0500

    Files: created apiRoutes.js and htmlRoutes.js. Required them in server.js. htmlRoutes.js should create the routes for notes and home.

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,23 @@
 // Dependencies
 // =============================================================
 var express = require("express");
+const path = require('path');
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
